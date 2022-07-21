ced60634a25aee0e968a118536b5892c218b6fc9
kuzmuknat <kuzmuknat@gmail.com>
2022-07-21 16:46:17 -0400

    Initial commit

---
+++ js_hw1/script.js
@@ -0,0 +1,81 @@
+let str1 = 'hello';
+let str2 = 'owu';
+let str3 = 'com';
+let str4 = 'ua';
+
+let num1 = 1;
+let num2 = 10;
+let num3 = -999;
+let num4 = 123;
+let num5 = 3.14;
+let num6 = 2.7;
+let num7 = 16;
+
+let bool1 = true;
+let bool2 = false;
+
+console.log(str1);
+console.log(str2);
+console.log(str3);
+console.log(str4);
+
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+console.log(num5);
+console.log(num6);
+console.log(num7);
+
+console.log(bool1);
+console.log(bool2);
+
+alert(str1);
+alert(str2);
+alert(str3);
+alert(str4);
+
+alert(num1);
+alert(num2);
+alert(num3);
+alert(num4);
+alert(num5);
+alert(num6);
+alert(num7);
+
+alert(bool1);
+alert(bool2);
+
+document.writeln(str1);
+document.writeln(str2);
+document.writeln(str3);
+document.writeln(str4);
+
+document.writeln(num1);
+document.writeln(num2);
+document.writeln(num3);
+document.writeln(num4);
+document.writeln(num5);
+document.writeln(num6);
+document.writeln(num7);
+
+document.writeln(bool1);
+document.writeln(bool2);
+
+
+let firstName = prompt("first name:");
+let middleName = prompt("middle name:");
+let lastName = prompt("last name:");
+
+let person = firstName +' '+ middleName +' '+lastName;
+console.log(person);
+
+let a = 100;
+let b = '100';
+let c = true;
+
+let person2;
+person2 = prompt("first name:");
+person2 = person2 +' '+ prompt("middle name:");
+person2 = person2 +' '+prompt("last name:");
+console.log(person2);
